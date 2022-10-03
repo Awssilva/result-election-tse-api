45e88f2bead2f25248b20e2e0e9e651b1d55e989
Alexandre Silva <93205517+Awssilva@users.noreply.github.com>
2022-10-03 15:36:59 -0300

    add candidatos, add refresh with clear screen

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 import json
 import pandas as pd
+import os
 from time import sleep
+from datetime import datetime, timedelta
+clear = lambda: os.system('clear') 
 
 data = requests.get(
     "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
@@ -14,14 +17,17 @@ votos = []
 porcentagem = []
 
 for infos in json_data['cand']:
-    if infos['seq'] in '1 2 3 4'.split(' '):
+    if infos['seq'] in '1 2 3 4 5 6 7 8 9 10'.split(' '):
         candidato.append(infos['nm'])
         votos.append(infos['vap'])
         porcentagem.append(infos['pvap'])
 while True:
   df_eleicao = pd.DataFrame(
-          list(zip(candidato, votos, porcentagem)),
-          columns=['Candidato', 'Nº votos', 'Porcentagem']
+  list(zip(candidato, votos, porcentagem)),
+  columns=['Candidato', 'Votos', 'Porcentagem']
   )
-  print(df_eleicao)
-  sleep(10)
+  to_gmt3 = datetime.now() - timedelta(hours=3)
+  print(f'{to_gmt3.strftime("%H:%M:%S")} - Aguarde\n')
+  print(df_eleicao,'\n')
+  sleep(30)
+  clear()
